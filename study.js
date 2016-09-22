d9d1ee18234780485db1d78ad738f1b16d92833d
404errrror <xotkd5972@naver.com>
2016-09-22 18:22:55 +0900

    Link soket, Add socket chatting.

--- study.js
+++ study.js
@@ -1,11 +1,29 @@
-var express = require('express');
-
-var app = express();
-
-app.get('/',function(req,res){
-    res.send('Hello World!');
+var io = require('socket.io')({
+    transports: ['websocket'],
 });
 
-app.listen(3000,function(){
-    console.log('Server On!');
+
+io.attach(3000);
+
+io.on('connection', function(socket) {
+    console.log('Server Connect!!');
+    socket.on('join',function (data){
+        console.log(data.user + ' : ' + data.roomname);
+
+        socket.leave(socket.room);
+        socket.join(data.roomname);
+        socket.room = data.rommname;
+    });
+
+
+    socket.on('chat',function (data) {
+        console.log(data.user + ' : ' + data.msg + ' : ' + data.dae);
+
+        io.socket.in(socket.room).emit('chat', data);
+    });
+
+    socket.on('disconnect',function(data){
+        console.log(data.user + 'disconnect!');
+    });
+
 });
